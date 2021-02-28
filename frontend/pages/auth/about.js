bf99896d94f813d9c178b5f7c051769cf76c1cbd
mariadias143 <marianovadias@gmail.com>
2021-02-28 12:11:47 +0000

    About

--- frontend/pages/auth/about.js
+++ frontend/pages/auth/about.js
@@ -26,12 +26,13 @@ import Auth from "../../layouts/Auth";
 function About() {
   return (
     <>
-      <Col lg="5" md="7">
-        <Card className="bg-secondary shadow border-0">
+      <Col lg="5" md="7" style={{width:'200vw'}}>
+        <Card className="bg-secondary shadow border-0" >
           
           <CardBody className="px-lg-5 py-lg-5">
             <div className="text-center text-muted mb-4">
-              <small>Efetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar LoginEfetuar Login</small>
+              <p>A Explica é um serviço solidário de apoio a alunos do ensino primário. Efetuando registo na aplicação, podes escolher uma Disciplina na qual tenhas dúvidas e efetuar um pedido de ajuda. Ser-te-á atribuído um Explicador voluntário na hora, com o qual podes trocar mensagens e agendar reuniões para resolver as tuas dúvidas. </p>
+              <p>Se queres ser um Explicador voluntário, basta efetuares o registo para ficares elegível a ser escolhido por um aluno.</p>
             </div>
             
           </CardBody>
